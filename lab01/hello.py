178dfa934bfcd8dca58e000b4ac896217b23c947
Michal Kepski <majkel.mk@gmail.com>
2017-03-17 12:17:09 +0100

    Update and rename hello_world.py to hello.py
    Uzupełniono skrypt o przykład używania parametrów podanych w trakcie uruchomienia programu przez użytkownika w wierszu poleceń.

---
+++ lab01/hello.py
@@ -0,0 +1,5 @@
+import sys
+if len(sys.argv) < 2:
+	print("Uruchamiajac skrypt podaj jako parametr swoje imie!")
+else:
+	print("Witaj " + sys.argv[1])
--- lab01/hello_world.py
+++
@@ -1,3 +0,0 @@
-print("""Hello world""")
-print("Hello again")
-print('Hello again')
